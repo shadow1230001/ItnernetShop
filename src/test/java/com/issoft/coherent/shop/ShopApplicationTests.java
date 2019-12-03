5cb5dac74d045bffcb29a9de8da7b9c2fe47e27c
VladimirLyubin <VladimirLyubin@coherentsolutions.com>
2019-12-03 11:10:13 +0300

    initial commit

---
+++ src/test/java/com/issoft/coherent/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issoft.coherent.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
